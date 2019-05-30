0991a3c059b986381fb7163ec0294d7661c4fc9a
Karol Bulwin <karolbulwin@gmail.com>
2019-05-30 11:19:41 +0200

    Navigate Between Pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <a title="About Page">About Page</a>
+    </Link>
+    <Link href="/about">
+      <button>Go to About Page</button>
+    </Link>
+
     <p>Hello Next.js</p>
     <input placeholder="Enter Some Text" />
   </div>
